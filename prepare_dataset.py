c8b9a6403249c2ec32cdc5e7163e68b79ceffb7d
Tang Wai Hoh <waihoh@gmail.com>
2022-08-29 23:00:30 +0800

    added

---
+++ prepare_dataset.py
@@ -0,0 +1,5 @@
+import torchvision
+import torch
+
+cifar_data = torchvision.datasets.CIFAR100(root='/content/', download=True)
+data_loader = torch.utils.data.DataLoader(cifar_data, batch_size=4, shuffle=True)
