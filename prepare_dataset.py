60e082d1bdc93fc50a851591e2aaeae67bfdd546
Tang Wai Hoh <waihoh@gmail.com>
2022-08-30 09:39:37 +0800

    updated

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,5 +1,6 @@
 import torchvision
 import torch
 
-cifar_data = torchvision.datasets.CIFAR100(root='/content/', download=True)
+# NOTE: cifar-100-python data folder is in content folder
+cifar_data = torchvision.datasets.CIFAR100(root='./content/', train=True, download=True)
 data_loader = torch.utils.data.DataLoader(cifar_data, batch_size=4, shuffle=True)
