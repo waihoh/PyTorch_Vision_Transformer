c1499c0fd5ae48016d8136ec401982c5f70d8c34
Tang Wai Hoh <waihoh@gmail.com>
2022-08-30 13:08:54 +0800

    updated. The code runs on GPU now.

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ from prepare_dataset import data_loader
 EPOCHS = 2
 LEARNING_RATE = 1e-3
 
-device = torch.device('cpu')
+device = torch.device('cpu')  # cuda:0
 
 vision_transformer = VisionTransformer(image_size=32).to(device)
 losses = train(vision_transformer, EPOCHS, LEARNING_RATE, data_loader, device)
 
-# The current code runs on cpu.
-# TODO: To test the code on GPU
 print(losses)
 
